576bd823b6305df1415154d13db360b387a867f5
training <253164597@qq.com>
2019-06-27 15:35:53 +0800

    add

--- dialog_text_edit.cpp
+++ dialog_text_edit.cpp
@@ -4,8 +4,8 @@
 dialog_text_edit::dialog_text_edit(QWidget *parent)
         : QDialog(parent),ui(new Ui::dialog_text_edit)
 {
-
-
+    ui->setupUi(this);
+    ui->plainTextEdit->appendPlainText(QString("xxx"));
 }
 dialog_text_edit::~dialog_text_edit()
 {
